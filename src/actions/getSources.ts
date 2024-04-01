96185a116b26a42568cbe48df393893608716746
shimul <theshimulh@gmail.com>
2024-04-01 22:16:43 +0600

    feat: middlewire

--- src/actions/getSources.ts
+++ src/actions/getSources.ts
@@ -8,9 +8,7 @@ export const getNewsSources = async () => {
   );
 
   const newsApiData = await newsAPiRes.json();
-
   const newsSources = newsApiData.sources?.slice(0, 6);
-  console.log('data--', newsSources);
 
   return {
     success: newsApiData.status,
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,7 +12,6 @@ export default async function page({
 }: {
   searchParams: { q: string; cat: string; source: string };
 }) {
-  console.log(searchParams);
   return (
     <div>
       <Search />
