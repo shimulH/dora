9d3fb200fe076b36489f39372d18cb304648ac85
shimul <theshimulh@gmail.com>
2024-04-02 08:34:42 +0600

    feat: metadata edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,6 @@ import SectionContainer from '@/components/section-container';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'Basic dashboard with Next.js and Shadcn',
-};
-
 export default async function RootLayout({
   children,
 }: {
